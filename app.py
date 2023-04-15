489046312e7f113c8ccf0f52a98aef04f0c62a04
Rutvik <rutvikm200@gmail.com>
2023-04-15 12:38:08 +0530

    update homepage

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request , render_template_string
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    html = """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>String Reverser API</title>
+        </head>
+        <body>
+            <h1>String Reverser API</h1>
+            <p>This API has one endpoint: <code>/reverse</code></p>
+            <p>To use the endpoint, send a <code>POST</code> request with a form field named <code>string</code> containing the string you want to reverse.</p>
+            <p>The endpoint will return the reverse of the input string.</p>
+        </body>
+    </html>
+    """
+    return render_template_string(html)
 
 @app.route('/reverse', methods=['POST'])
 def reverse_string():
